fd8fa3ecbbb5edd4413e90b490334a5e0f240e74
Antti Nikku <antti.nikku@icloud.com>
2019-09-07 11:41:59 +0300

    -Lisätty alertdialogi showlist näkymään. -Kysyy käyttäjältä haluaako palata takaisin vai pysytäänkö tällä sivulla

--- ShoppingList/app/src/main/java/com/example/shoppinglist/ShowList.java
+++ ShoppingList/app/src/main/java/com/example/shoppinglist/ShowList.java
@@ -1,9 +1,12 @@
 package com.example.shoppinglist;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,19 +14,70 @@ import android.widget.Toast;
 public class ShowList extends AppCompatActivity {
 
 
+    private Button back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_list);
 
-        // Get the Intent that started this activity and extract the string
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
-        // Capture the layout's TextView and set the string as its text
+
         TextView textView = findViewById(R.id.textList);
         textView.setText(message);
 
+        this.back = findViewById(R.id.back);
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(ShowList.this)
+                        .setTitle("Palaa edelliseen näkymään?")
+                        .setMessage("Oletko varma?")
+                        .setPositiveButton("Kyllä", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                // The user wants to leave - so dismiss the dialog and exit
+                                finish();
+                                dialog.dismiss();
+                            }
+                        }).setNegativeButton("Ei", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // The user is not sure, so you can exit or just stay
+                        dialog.dismiss();
+                    }
+                }).show();
+            }
+        });
+
+
     }
-    
+
+    @Override
+    public void onBackPressed() {
+        // Here you want to show the user a dialog box
+
+
+        /*
+        new AlertDialog.Builder(getApplicationContext())
+                .setTitle("Exiting the App")
+                .setMessage("Are you sure?")
+                .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // The user wants to leave - so dismiss the dialog and exit
+                        finish();
+                        dialog.dismiss();
+                    }
+                }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // The user is not sure, so you can exit or just stay
+                dialog.dismiss();
+            }
+        }).show();
+        */
+    }
 }
